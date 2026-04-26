62d3446b8f20e95260932d35bc0ee45e0156b127
aryan Tanwani <165964757+atanwi201@users.noreply.github.com>
2026-04-26 22:05:55 +0100

    Refactor weather and quote fetching logic and adding button

--- anime_microdisplay/anime_microdisplay.ino
+++ anime_microdisplay/anime_microdisplay.ino
@@ -3,7 +3,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -13,22 +12,29 @@
 #define SCREEN_ADDRESS 0x3C
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-//sda21 sdc22
+//SDA21 SCL22
 // WiFi Configuration
-const char* ssid = "YOUR_WIFI_NAME";
-const char* password = "YOUR_WIFI_PSWD";
-
+const int quotes_len = 550;
+const int btn_pin = 18;
+const char* ssid = "Your_wifi";
+const char* password = "Your_password";
+int btn_state;
 // URLs
-char str[150] ;//this is a variable that will be changed for the link of the "api" //change the longitute and latitude
+char str[150] ;//this is a variable that will be changed for the link of the "api"
+//remember to change the Weather API place
 const char* url_weather = "https://api.open-meteo.com/v1/forecast?latitude=28.4682&longitude=-16.2546&current=temperature_2m,precipitation,is_day&timezone=auto";
 
-HTTPClient http;
 
+int counter = 0;
 String quote = "";
 String character = "";
-int counter = 0;
-
+String currentTime;
+String temp;
+int isDay;
+float precip;
+//component functions
 void getQuote();
+void getWeather();
 
 void setup() {
   Serial.begin(115200);
@@ -54,8 +60,9 @@ void setup() {
 
   Serial.println("\nCONNECTED to SSID: " + String(ssid));
   // set randomness
-  srand(time(NULL));
-
+  randomSeed(esp_random());
+  Serial.print(time(NULL));
+  pinMode(btn_pin, INPUT);
   display.clearDisplay();
   display.setCursor(0, 0);
   display.println("Connected to:");
@@ -65,59 +72,42 @@ void setup() {
 }
 
 void loop() {
+  btn_state = digitalRead(btn_pin);
+
+  if(btn_state == 1)
+  {
+    Serial.print("btn pressed");
+    getQuote();
+    delay(100);
+  }else{
+    Serial.print(btn_state);
+  }
   display.clearDisplay();
   display.setCursor(0, 0);
 
   if (WiFi.status() == WL_CONNECTED) {
-    // Refresh quote every hour (3600 seconds)
-    if (counter == 0 || counter >= 3) {
+    // Refresh quote every 30s
+    if (counter == 0 || counter >= 30) {
       counter = 0;
       getQuote();
     }
-
-    // Get weather
-    http.begin(url_weather);
-    int httpCode = http.GET();
-
-    if (httpCode > 0) {
-      String payload = http.getString();
-
-      StaticJsonDocument<1024> doc;  // Smaller size is enough for weather
-      DeserializationError error = deserializeJson(doc, payload);
-
-      if (error) {
-        Serial.print("JSON deserialize error (weather): ");
-        Serial.println(error.c_str());
-        display.println("Weather JSON error");
-      } else {
-        JsonObject current = doc["current"];
-        Serial.print(current);
-        String time = current["time"].as<String>();
-        String temp = current["temperature_2m"].as<String>();
-        int isDay = current["is_day"];
-        float precip = current["precipitation"];
+    if (counter == 0 || counter >= 10) {
+      getWeather();
+    }
 
         // Display everything
         display.setTextSize(1);
-        display.print(time + " ");
+        display.print(currentTime + " ");
         display.println(isDay ? "Day" : "Night");  // Show Day or Night
         display.print(temp);
         display.print("C - ");
-                if (precip > 0.0) {
+              if (precip > 0.0) {
           display.println("Raining...");
         } else {
           display.println("No rain");
         }
         display.println(quote);
         display.println("- " + character);
-
-      }
-    } else {
-      display.println("HTTP error (weather)");
-      Serial.println("HTTP error: " + String(httpCode));
-    }
-
-    http.end();
   } else {
     display.println("WiFi disconnected");
   }
@@ -125,11 +115,12 @@ void loop() {
   display.display();
   counter++;
   Serial.println("Counter: " + String(counter));
-  delay(10*1000);  // Update every second
+  delay(1*1000);  // Update every 1' second
 }
 
 void getQuote() {
-  int num = rand() % 250 + 1;
+  HTTPClient http;
+  int num = random(1, quotes_len + 1);
 
   snprintf(str, sizeof(str),
            "https://the-system-201.neocities.org/home/quotes/json_full_chunk_%d.json",
@@ -169,6 +160,39 @@ void getQuote() {
 
   http.end();
 }
+
+void getWeather()
+{
+  HTTPClient http;
+  http.begin(url_weather);
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      String payload = http.getString();
+
+      StaticJsonDocument<1024> doc;  // Smaller size is enough for weather
+      DeserializationError error = deserializeJson(doc, payload);
+
+      if (error) {
+        Serial.print("JSON deserialize error (weather): ");
+        Serial.println(error.c_str());
+        display.println("Weather JSON error");
+      } else {
+        JsonObject current = doc["current"];
+        Serial.print(current);
+        currentTime = current["time"].as<String>();
+        temp = current["temperature_2m"].as<String>();//because it's a floating number
+        isDay = current["is_day"];
+        precip = current["precipitation"];
+        }
+    } else {
+      display.println("HTTP error (weather)");
+      Serial.println("HTTP error: " + String(httpCode));
+    }
+
+    http.end();
+}
+
 
 
 
